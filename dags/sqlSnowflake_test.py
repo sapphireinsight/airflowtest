a9ae848e85532b6466c81601e43a520033c46859
kittenPlayground <131211409+kittenPlayground@users.noreply.github.com>
2023-11-07 17:35:25 -0800

    Update sqlSnowflake_test.py

--- dags/sqlSnowflake_test.py
+++ dags/sqlSnowflake_test.py
@@ -1,13 +1,11 @@
 from airflow import DAG
-from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
 from hello_plugin.operators.simple_operator import simpleOperator
 
+from airflow.operators.python_operator import PythonOperator
+from jnius import autoclass
+
 SNOWFLAKE_CONN_ID = 'simple-test-conn'
-# SNOWFLAKE_SCHEMA = 'INSIGHTS'
-# SNOWFLAKE_WAREHOUSE = 'B2B_INSIGHTS_COMPUTE_WH'
-# SNOWFLAKE_DATABASE = 'B2B_111BBB999_DB'
-# SNOWFLAKE_ROLE = 'B2B_INSIGHTS_111BBB999_WRITE_USER'
 
 
 MYSQL_CONNECTION_ID = 'mysql_297_mti_476'
@@ -19,27 +17,72 @@ default_args = {
   'owner': 'airflow',
 }
 
-dag = DAG(
+# dag = DAG(
+#     'sql_snowflake_test',
+#     default_args=default_args,
+#     # schedule_interval="@daily",
+#     # start_date=datetime(2021, 1, 1),
+#     # catchup=False,
+#     schedule_interval='*/5 * * * *', dagrun_timeout=timedelta(seconds=5), start_date=datetime(2023, 1, 1), catchup=False,
+#     tags=['example'],
+# )
+#
+# sql_snowflake_op_with_params = simpleOperator(
+#     task_id='sql_snowflake_op_with_params',
+#     dag=dag,
+#     snowflake_conn_id=SNOWFLAKE_CONN_ID,
+#     sql_conn_id=MYSQL_CONNECTION_ID,
+#     sql_query=MYSQL_QUERY,
+#     sql_database=MYSQL_DATABASE,
+# )
+#
+# sql_snowflake_op_with_params
+
+
+def run_some_java_codes():
+  Stack = autoclass('java.util.Stack')
+  stack = Stack()
+  stack.push('hello')
+  stack.push('world')
+  print(stack.pop()) # --> 'world'
+  print(stack.pop()) # --> 'hello'
+
+
+with DAG(
     'sql_snowflake_test',
     default_args=default_args,
     # schedule_interval="@daily",
     # start_date=datetime(2021, 1, 1),
     # catchup=False,
-    schedule_interval='0 0 1 * *', dagrun_timeout=timedelta(seconds=5), start_date=datetime(2023, 1, 1), catchup=False,
+    schedule_interval='*/5 * * * *', dagrun_timeout=timedelta(seconds=5), start_date=datetime(2023, 1, 1), catchup=False,
     tags=['example'],
-)
+):
 
-sql_snowflake_op_with_params = simpleOperator(
-    task_id='sql_snowflake_op_with_params',
-    dag=dag,
-    snowflake_conn_id=SNOWFLAKE_CONN_ID,
-    # database=SNOWFLAKE_DATABASE,
-    # warehouse=SNOWFLAKE_WAREHOUSE,
-    # schema=SNOWFLAKE_SCHEMA,
-    # role=SNOWFLAKE_ROLE,
-    sql_conn_id=MYSQL_CONNECTION_ID,
-    sql_query=MYSQL_QUERY,
-    sql_database=MYSQL_DATABASE,
-)
+  test_run_java_code = PythonOperator(
+      task_id="test_run_java_code",
+      python_callable=run_some_java_codes,
+      op_kwargs={},
+      provide_context=True,
+  )
 
-sql_snowflake_op_with_params
+  sync_activity_type = simpleOperator(
+      task_id='write_activity_type',
+      snowflake_conn_id=SNOWFLAKE_CONN_ID,
+      sql_conn_id=MYSQL_CONNECTION_ID,
+      sql_query=MYSQL_QUERY,
+      sql_database=MYSQL_DATABASE,
+      sql_table="activity_type_temp_2",
+      sql_table_columswithtype="id integer, name string"
+  )
+
+  sync_smart_list_rule = simpleOperator(
+      task_id='write_smart_list_rule',
+      snowflake_conn_id=SNOWFLAKE_CONN_ID,
+      sql_conn_id=MYSQL_CONNECTION_ID,
+      sql_query=MYSQL_QUERY,
+      sql_database=MYSQL_DATABASE,
+      sql_table="smart_list_rule",
+      sql_table_columswithtype="id integer, target_type string, operator string, target_activity_type_id integer, conditions string"
+  )
+
+  test_run_java_code >> [sync_activity_type, sync_smart_list_rule]
