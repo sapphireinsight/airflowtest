72f04b5072431aa89fb3884f2b2bbf39750f0938
kittenPlayground <131211409+kittenPlayground@users.noreply.github.com>
2023-11-08 21:02:30 -0800

    Create run_jar.py

---
+++ dags/run_jar.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators import BashOperator
+from datetime import datetime
+import os
+import sys
+
+
+with DAG(
+    dag_id="runjar",
+    schedule_interval="@daily",
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+):
+  run_jar_task= BashOperator(
+    task_id = 'runjar',
+    bash_command = 'java -cp /data/mongoConnectionTest-1.1-SNAPSHOT-shaded.jar Calculator'
+  )
+  run_jar_task
